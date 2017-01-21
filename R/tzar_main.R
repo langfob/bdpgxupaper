4fe6e2607b4fc1cb12f4c13765e6000cf76d068f
Bill Langford <bill.langford@rmit.edu.au>
2017-01-21 12:12:58 +1100

    Make convenience calls of tzar for both inside and outside package

--- R/tzar_main.R
+++ R/tzar_main.R
@@ -4,16 +4,22 @@
 #
 #===============================================================================
 
-#' Convenience function to run tzar with all appropriate variables set
+#' Convenience function to run tzar in pkg with all appropriate variables set
+#'
+#' The name stands for "run Tzar Inside Package".  The function is intended to
+#' be quick and easy to call over and over from the command line, so its
+#' name is short and the call has no arguments.
+#' You have to modify the arguments
+#' set inside the function to correspond to your situation.
 #'
 #' @return results of running tzar
 #' @export
 #'
 #' @examples \dontrun{
-#' runt ()
+#' runtip ()
 #'}
 
-runt <- function ()
+runtip <- function ()
     {
     emulating_tzar                     = TRUE
     main_function                      = tzar_main
@@ -46,6 +52,55 @@ runt <- function ()
 
 #-------------------------------------------------------------------------------
 
+#' Convenience function to run tzar outside pkg with all appropriate variables set
+#'
+#' The name stands for "run Tzar Outside Package".  The function is intended to
+#' be quick and easy to call over and over from the command line, so its
+#' name is short and the call has no arguments.
+#' You have to modify the arguments
+#' set inside the function to correspond to your situation.
+#'
+#' @return results of running tzar
+#' @export
+#'
+#' @examples \dontrun{
+#' runtop ()
+#'}
+
+runtop <- function ()
+    {
+    emulating_tzar                     = TRUE
+    main_function                      = tzar_main
+    project_path                       = "/Users/bill/D/Projects/ProblemDifficulty/pkgs/bdpgxupaper/R"
+    emulation_scratch_file_path        = "~/tzar_emulation_scratch.yaml"
+    tzar_jar_path                      = "~/D/rdv-framework-latest-work/tzar.jar"
+
+    copy_model_R_tzar_file             = FALSE
+
+    model_R_tzar_src_dir               = NULL
+    model_R_tzar_disguised_filename    = NULL
+    overwrite_existing_model_R_dest    = NULL
+    required_model_R_filename_for_tzar = NULL
+
+    return (
+        tzar::run_tzar (
+              emulating_tzar                     = emulating_tzar,
+              main_function                      = main_function,
+              project_path                       = project_path,
+              emulation_scratch_file_path        = emulation_scratch_file_path,
+              tzar_jar_path                      = tzar_jar_path,
+
+              copy_model_R_tzar_file             = copy_model_R_tzar_file,
+              model_R_tzar_src_dir               = model_R_tzar_src_dir,
+              model_R_tzar_disguised_filename    = model_R_tzar_disguised_filename,
+              overwrite_existing_model_R_dest    = overwrite_existing_model_R_dest,
+              required_model_R_filename_for_tzar = required_model_R_filename_for_tzar
+              )
+            )
+    }
+
+#===============================================================================
+
 #' Wrapper function to call your application code from tzar
 #'
 #' @param parameters  list of parameters built by tzar from project.yaml
@@ -58,9 +113,12 @@ runt <- function ()
 
 tzar_main <- function (parameters)
     {
+
+        #  REPLACE THIS EXAMPLE CODE WITH YOUR CODE THAT YOU WANT TZAR TO RUN.
     cat ("\n\nIn tzar_main now.  parameters = \n\n")
     print (parameters)
     cat ("\n\nAll done now...\n\n")
+
     }
 
 #===============================================================================
