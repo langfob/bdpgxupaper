2a2ab3ef9a8862643566a311e75ec93f3d8c0a93
Bill Langford <bill.langford@rmit.edu.au>
2017-05-16 11:16:33 +1000

    Restructure mainline to add gen of APP prob and graph measures for base COR prob

--- R/xu_paper_main.R
+++ R/xu_paper_main.R
@@ -128,65 +128,109 @@ xu_paper_main = function (parameters)
     parameters             <- params_and_error_codes$parameters
     bdpg_error_codes       <- params_and_error_codes$bdpg_error_codes
 
-#===============================================================================
-#       Generate a problem, i.e, create the Xu graph nodes and edge_list.
-#===============================================================================
-
-    COR_bd_prob = bdpg::gen_bdprob (parameters,
-                               bdpg_error_codes,
-                               bdpg::get_integerize_function (parameters$integerize_string)
-                               # ,
-                               # DEBUG_LEVEL
-                               )
-
-    bdpg::do_COR_marxan_analysis_and_output (COR_bd_prob, parameters)
-
-    cat("\n\njust after set_up_for_and_run_marxan() for cor problem")
-    cat ("\n\n================================================================================")
-    cat ("\n================================================================================\n\n")
-
-#-------------------------------------------------------------------------------
-
-    #     #  Try running marxan on the base cor problem since we know that
-    #     #  this particular test has created a wrapped problem.
-    #     #
-    #     #  Note that a different way around this issue is to create the
-    #     #  cor base problem and do the analysis on it before creating
-    #     #  the wrapped problem, but creating the wrap by directly passing
-    #     #  the cor to it without going through gen_bdprob's superstructure.
-    #
-    # get_base_cor_prob_of_wrap_prob <- function ()
-    #     {
-    #     cat ("not implemented yet""
-    #     }
-    # base_COR_bd_prob = get_base_cor_prob_of_wrap_prob (COR_bd_prob)
-    #
-    # base_COR_marxan_ret_values = bdpg::set_up_for_and_run_marxan_COR (base_COR_bd_prob, parameters)
-    #
-    # marxan_control_values  = base_COR_marxan_ret_values$marxan_control_values
-    # base_COR_bd_prob       = base_COR_marxan_ret_values$base_COR_bd_prob  #  COR_bd_prob has new dirs
-    #
-    # cat("\n\njust after set_up_for_and_run_marxan() for base cor problem")
+    integerize             <- bdpg::get_integerize_function (parameters$integerize_string)
 
 #===============================================================================
-#  Generate an APPARENT problem from the correct problem, i.e., apply errors.
+#       Generate a base problem, i.e, create the Xu graph nodes and edge_list.
+#===============================================================================
+
+        #  May need to test these parameters to see if they even exist
+        #  and if they don't for this particular run, then set them to
+        #  something like NULL.
+
+    infile_name = parameters$infile_name
+    if (is.null (infile_name)) infile_name = ""
+
+    starting_dir = file.path (normalizePath (parameters$fullOutputDirWithSlash))
+
+    base_COR_bd_prob = bdpg::gen_single_bdprob_COR (starting_dir,
+                                                      parameters,
+                                                      parameters$read_Xu_problem_from_Xu_file,
+                                                      infile_name,
+                                                      parameters$given_correct_solution_cost,
+                                                      parameters$max_allowed_num_spp,
+                                                      bdpg_error_codes,
+                                                      integerize)
+
+#===============================================================================
+#  Generate an APPARENT problem from the base problem, i.e., apply errors.
 #===============================================================================
 
     if (parameters$apply_error_to_spp_occupancy_data)
         {
-        APP_bd_prob = bdpg::gen_single_bdprob_APP (COR_bd_prob,
-                                                #COR_bd_prob@top_outdir,
-                                                parameters,
-                                                bdpg_error_codes,
-                                                bdpg::get_integerize_function (parameters$integerize_string)
-                                                )
+        base_APP_bd_prob = bdpg::gen_single_bdprob_APP (base_COR_bd_prob,
+                                                        parameters,
+                                                        bdpg_error_codes,
+                                                        integerize)
 
-        bdpg::do_APP_marxan_analysis_and_output (APP_bd_prob, COR_bd_prob, parameters)
+        bdpg::do_APP_marxan_analysis_and_output (base_APP_bd_prob,
+                                                 base_COR_bd_prob,
+                                                 parameters)
 
-        cat("\n\njust after set_up_for_and_run_marxan() for app problem")
+        cat("\n\njust after set_up_for_and_run_marxan() for base app problem")
         cat ("\n\n================================================================================")
         cat ("\n================================================================================\n\n")
         }
+
+#===============================================================================
+#       Generate a wrapped problem around the base problem.
+#===============================================================================
+
+    if (parameters$wrap_lognormal_dist_around_Xu)
+        {
+        wrapped_COR_bd_prob = bdpg::gen_bdprob (parameters,
+                                                bdpg_error_codes,
+                                                integerize,
+                                                base_COR_bd_prob)
+
+        bdpg::do_COR_marxan_analysis_and_output (wrapped_COR_bd_prob, parameters)
+
+        cat("\n\njust after set_up_for_and_run_marxan() for wrapped cor problem")
+        cat ("\n\n================================================================================")
+        cat ("\n================================================================================\n\n")
+
+    #-------------------------------------------------------------------------------
+
+        #     #  Try running marxan on the base cor problem since we know that
+        #     #  this particular test has created a wrapped problem.
+        #     #
+        #     #  Note that a different way around this issue is to create the
+        #     #  cor base problem and do the analysis on it before creating
+        #     #  the wrapped problem, but creating the wrap by directly passing
+        #     #  the cor to it without going through gen_bdprob's superstructure.
+        #
+        # get_base_cor_prob_of_wrap_prob <- function ()
+        #     {
+        #     cat ("not implemented yet""
+        #     }
+        # base_COR_bd_prob = get_base_cor_prob_of_wrap_prob (COR_bd_prob)
+        #
+        # base_COR_marxan_ret_values = bdpg::set_up_for_and_run_marxan_COR (base_COR_bd_prob, parameters)
+        #
+        # marxan_control_values  = base_COR_marxan_ret_values$marxan_control_values
+        # base_COR_bd_prob       = base_COR_marxan_ret_values$base_COR_bd_prob  #  COR_bd_prob has new dirs
+        #
+        # cat("\n\njust after set_up_for_and_run_marxan() for base cor problem")
+
+    #===============================================================================
+    #  Generate an APPARENT problem from the wrapped problem, i.e., apply errors.
+    #===============================================================================
+
+        if (parameters$apply_error_to_spp_occupancy_data)
+            {
+            wrapped_APP_bd_prob = bdpg::gen_single_bdprob_APP (wrapped_COR_bd_prob,
+                                                                #COR_bd_prob@top_outdir,
+                                                                parameters,
+                                                                bdpg_error_codes,
+                                                                integerize)
+
+            bdpg::do_APP_marxan_analysis_and_output (APP_bd_prob, COR_bd_prob, parameters)
+
+            cat("\n\njust after set_up_for_and_run_marxan() for app problem")
+            cat ("\n\n================================================================================")
+            cat ("\n================================================================================\n\n")
+            }
+        }  #  end if - wrap_lognormal_dist_around_Xu
 
 #===============================================================================
 #               Clean up tzar, console sink, etc.
