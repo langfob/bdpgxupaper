4f2a02dd93fc98b52ba850ebdfdbd70f4c7a0da5
Bill Langford <bill.langford@rmit.edu.au>
2017-08-08 11:20:02 +1000

    Add runt() to replace runtip(), runtop(), and runtwoe()

--- R/tzar_main.R
+++ R/tzar_main.R
@@ -29,3 +29,27 @@ tzar_main <- function (parameters)
     }
 
 #===============================================================================
+
+#' Convenience function to call run_tzar without having to enter arguments
+#'
+#' @param main_function  name of function to call to run under tzar or tzar emulation
+#' @param parameters_yaml_file_path Full path with file name for the
+#' project.yaml file
+#' @param tzar_emulation_yaml_file_path Full path with file name for the
+#' yaml file containing control values for tzar emulation
+#'
+#' @return Returns nothing
+#' @export
+#'
+#' @examples \dontrun{
+#' runt ()
+#'}
+
+runt <- function ()
+    {
+    tzar::run_tzar (main_function = tzar_main,
+              parameters_yaml_file_path = "./R/project.yaml",
+              tzar_emulation_yaml_file_path = "./R/tzar_emulation.yaml")
+    }
+
+#===============================================================================
